639ff64402c1644b5951a9db6eeaa18e8690f222
Shah rahman <toshah.rahman@gmail.com>
2022-03-01 21:14:03 +0200

    forgot password e2e-update

--- cypress/integration/e2e_tests/forgot-password.spec.js
+++ cypress/integration/e2e_tests/forgot-password.spec.js
@@ -2,20 +2,20 @@
 
 describe('e2e test scenario 2- forgot password', () => {
     it('visit site and click on the sign-in button', () => {
-      cy.visit('http://zero.webappsecurity.com/index.html')
-      cy.get('#signin_button').click()
+        cy.visit('http://zero.webappsecurity.com/index.html')
+        cy.get('#signin_button').click()
 
     })
     it('should click on the forgot password', () => {
-        
+        cy.get('offset3 > a').click()       
     })
 
     it('should fill email form', () => {
-        
+        cy.get('#user_email').type('a@haha.com')
     })
 
     it('should submit the form', () => {
-        
+        cy.get()
     })
 
 

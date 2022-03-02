ff9614b4b668431ad48cfa45f3d8d4db4c1b5f67
Shah rahman <toshah.rahman@gmail.com>
2022-03-02 17:09:39 +0200

    login logout e2e tests setup

---
+++ cypress/integration/e2e_tests/login-logout.spec.js
@@ -0,0 +1,23 @@
+/// <reference types="cypress" />
+
+describe('Login Logout e2e test', () => {
+    it('visit website', () => {
+        //code
+    })
+    
+    it('should try with invalid credentials', () => {
+        //code
+    })
+
+    it('should display error message', () => {
+        //code
+    })
+
+    it('should try with valid credentials', () => {
+        //code
+    })
+
+    it('should try logout', () => {
+        //code
+    })
+})

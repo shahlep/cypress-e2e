ac51bb263e9d62bb9b5b53fc0670953d700c7031
Shah rahman <toshah.rahman@gmail.com>
2022-03-02 21:02:32 +0200

    new payee e2e tests with modification

--- cypress/integration/e2e_tests/new-payee.spec.js
+++ cypress/integration/e2e_tests/new-payee.spec.js
@@ -1,20 +1,37 @@
 /// <reference types="cypress" />
 
 describe('New Payee e2e tests', () => {
-  
-    it('visit website', () => {
+    before(() => {
         cy.visit('http://zero.webappsecurity.com/index.html')
         cy.url().should('include','index.html')
-    })
-
-    it('should login with valid credentials', () => {
         cy.get('#signin_button').click()
         cy.url().should('include','login.html')
         cy.get('#user_login').type('username')
         cy.get('#user_password').type('password')
         cy.contains('Sign in').click()
-        cy.url().should('include','account-summary.html')
     })
 
+    //it('should be in account summary page', () => {
+       
+   //     cy.url().should('include','account-summary.html')
+     //   cy.contains('Pay Bills', {timeout: 10000}).click()
+    //})
+
+    it('should be in pay bills page, fill up the form and submit', () => {
+        cy.contains('Pay Bills', {timeout: 10000}).click()
+        cy.url().should('include','pay-bills.html')
+        cy.contains('Add New Payee' ,{timeout:10000}).click()
+        //cy.url().should('include','pay-bills-new-payee.html')
+        cy.get('#np_new_payee_name').type('test')
+        cy.get('#np_new_payee_address').type('test street 123')
+        cy.get('#np_new_payee_account').type('basic')
+        cy.get('#np_new_payee_details').type('text test')
+        cy.get('#add_new_payee').click()
+    })
+
+    it('should show user created msg', () => {
+        cy.get('#alert_content').should('be.visible').and('contain','The new payee test was successfully created.')
+
+    })
 
 })

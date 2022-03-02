d3e29ee89a8d10f5441640483a61259173b4812e
Shah rahman <toshah.rahman@gmail.com>
2022-03-02 06:52:07 +0200

    navbar e2e steps

--- cypress/integration/e2e_tests/navbar.spec.js
+++ cypress/integration/e2e_tests/navbar.spec.js
@@ -2,11 +2,14 @@
 
 describe('navbar e2e', () => {
     it('visit webiste', () => {
-      //code  
+      cy.visit('http://zero.webappsecurity.com/index.html')
+      cy.url().should('include','index.html')
     })
 
     it('should display online banking content', () => {
-        //code
+        cy.contains('Online Banking').click()
+        cy.url().should('include','online-banking.html')
+        cy.get('h1').should('be.visible')
     })
 
     it('should display feedback content', () => {

6140fba7d13d6332905d39c803a92219c32f8cf2
Shah rahman <toshah.rahman@gmail.com>
2022-03-02 06:41:56 +0200

    navbar e2e setup

---
+++ cypress/integration/e2e_tests/navbar.spec.js
@@ -0,0 +1,20 @@
+/// <reference types="cypress" />
+
+describe('navbar e2e', () => {
+    it('visit webiste', () => {
+      //code  
+    })
+
+    it('should display online banking content', () => {
+        //code
+    })
+
+    it('should display feedback content', () => {
+      //code  
+    })
+
+    it('should display homepage content', () => {
+        //code  
+      })
+    
+})

d65c7bc1e1685a35309cafcfb4c6e4a77349bdc2
Shah rahman <toshah.rahman@gmail.com>
2022-03-02 07:10:19 +0200

    feedback form e2e setup

---
+++ cypress/integration/e2e_tests/feedback-form.spec.js
@@ -0,0 +1,27 @@
+/// <reference types="cypress" />
+
+describe('Feedback form e2e', () => {
+  it('visit webiste', () => {
+      //code
+  }) 
+
+  it('should display feedback content', () => {
+      //code
+  })
+
+  it('should load feedback form', () => {
+      //code
+  })
+
+  it('should fill the feedback form', () => {
+      //code
+  })
+
+  it('should submit feedback form', () => {
+      //code
+  })
+
+  it('should display feedback message', () => {
+      //code
+  })
+})

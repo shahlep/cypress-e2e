3bc3d9e25d01180ab0836ad2717b8459c449f44a
Shah rahman <toshah.rahman@gmail.com>
2022-03-03 19:05:50 +0200

    find transactions e2e tests step 1

--- cypress/integration/e2e_tests/find-transactions.spec.js
+++ cypress/integration/e2e_tests/find-transactions.spec.js
@@ -11,6 +11,13 @@ describe('Find transactions e2e tests', () => {
     })
     
     it('should be in account activity page and find transactions', () => {
-        cy.contains('Account Activity').click() 
+        cy.contains('Account Activity').click()
+        cy.contains('Find Transactions').click()
+        //cy.get('#aa_description').type('test text')
+        //cy.get('#aa_fromDate').type('2022-01-01 {enter}')
+        //cy.get('#aa_toDate').type('2022-03-03 {enter}')
+        cy.get('#aa_fromAmount').type('50')
+        cy.get('#aa_toAmount').type('2000')
+        cy.get('button[type="submit"]').click()
     })
 })

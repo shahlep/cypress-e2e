f2c646729187bee332f57cb1a864a491925cb525
Shah rahman <toshah.rahman@gmail.com>
2022-03-03 18:31:25 +0200

    transfer funds e2e setup and step 2

--- cypress/integration/e2e_tests/transfer-fund.spec.js
+++ cypress/integration/e2e_tests/transfer-fund.spec.js
@@ -3,6 +3,7 @@
 describe('transfer funds e2e tests', () => {
   
     before(() => {
+        //mock website
         cy.visit('http://zero.webappsecurity.com/index.html')
         cy.url().should('include','index.html')
         cy.get('#signin_button').click()
@@ -12,5 +13,12 @@ describe('transfer funds e2e tests', () => {
 
     it('should be in transfer fund page,', () => {
       cy.contains('Transfer Funds').click()
+      cy.get('#tf_fromAccountId').select('3')
+      cy.get('#tf_toAccountId').select('2')
+      cy.get('#tf_amount').type('500')
+      cy.get('#tf_description').type('test text test text test text')
+      cy.get('#btn_submit').click()
+      cy.get('h2').should('be.visible').and('contain','Transfer Money & Make Payments - Verify')
+      cy.get('#btn_submit').click()
   }) 
 })

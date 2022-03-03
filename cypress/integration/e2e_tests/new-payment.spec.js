4e84540f3d3d853c6732d6a1078566601b010f7f
Shah rahman <toshah.rahman@gmail.com>
2022-03-03 06:37:21 +0200

    new payment e2e step 1

--- cypress/integration/e2e_tests/new-payment.spec.js
+++ cypress/integration/e2e_tests/new-payment.spec.js
@@ -3,6 +3,7 @@
 describe('New Payment e2e tests', () => {
   
     before(() => {
+        //mock website
         cy.visit('http://zero.webappsecurity.com/index.html')
         cy.url().should('include','index.html')
         cy.get('#signin_button').click()
@@ -10,5 +11,16 @@ describe('New Payment e2e tests', () => {
         cy.login('username','password')
     })
 
-    
+    it('should send new payment', () => {
+         cy.get('#pay_bills_tab').click()
+         cy.contains('Pay Saved Payee').click()
+         cy.get('#sp_payee').select('wellsfargo')
+         cy.get('#sp_account').select('3')
+         cy.get('#sp_amount').type('2')
+         cy.get('#sp_date').type('2022-03-03 {enter}')
+         cy.get('#sp_description').type('what you see is what you get')
+         cy.get('#pay_saved_payees').click()
+    })
+
+
 })
